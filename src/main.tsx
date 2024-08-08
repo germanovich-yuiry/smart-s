18e24515a88c6f14e9d618d9c7d8c0bc63ab0801
YuriyEG <you@example.com>
2024-08-09 09:59:02 +1200

    +

--- src/main.tsx
+++ src/main.tsx
@@ -12,6 +12,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
       <FontStyles />
+      <h1>s</h1>
       <App />
       <GlobalStyles />
     </Provider>
